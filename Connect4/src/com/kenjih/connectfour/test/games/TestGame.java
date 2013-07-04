3524e2960139984ba114a0a94fc19cf7f101ebe8
KenjiH <kenjih@goodpreparations.co.cc>
2013-07-04 23:30:18 +0900

    ver 0.0.3 added testcases for Game.java and GreadyPlayer.java

---
+++ Connect4/src/com/kenjih/connectfour/test/games/TestGame.java
@@ -0,0 +1,124 @@
+package com.kenjih.connectfour.test.games;
+
+import static org.junit.Assert.*;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.kenjih.connectfour.main.games.Game;
+import com.kenjih.connectfour.main.players.RandomPlayer;
+
+public class TestGame {
+
+	Game game;
+
+	@Before
+	public void setUp() throws Exception {
+		game = new Game(new RandomPlayer(), new RandomPlayer());
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	private void setBoard(char[][] board) throws Exception {
+		Field field = game.getClass().getDeclaredField("board");
+		field.setAccessible(true);
+		field.set(game, board);		
+	}
+	
+	private boolean winOf(char stone) throws Exception {
+		Method method = game.getClass().getDeclaredMethod("winOf", char.class);
+		method.setAccessible(true);
+		return (Boolean) method.invoke(game, stone);
+	}
+
+	@Test
+	public void testWinOf_1() throws Exception {
+		
+		char[][] board = { 
+			".......".toCharArray(),
+			".......".toCharArray(),
+			"..XOOO.".toCharArray(),
+			"..XXOO.".toCharArray(),
+			"..X.X..".toCharArray(), 
+			".......".toCharArray()
+		};
+		
+		setBoard(board);
+		assertEquals(false, winOf('O'));
+		assertEquals(false, winOf('X'));
+	}
+
+	@Test
+	public void testWinOf_2() throws Exception {
+		
+		char[][] board = { 
+			".......".toCharArray(),
+			".......".toCharArray(),
+			"..O....".toCharArray(),
+			"..O....".toCharArray(),
+			"..O....".toCharArray(), 
+			"..O....".toCharArray()
+		};
+		
+		setBoard(board);
+		assertEquals(true, winOf('O'));
+		assertEquals(false, winOf('X'));
+	}
+
+	@Test
+	public void testWinOf_3() throws Exception {
+		
+		char[][] board = { 
+			".......".toCharArray(),
+			".......".toCharArray(),
+			"..XXXX.".toCharArray(),
+			".......".toCharArray(),
+			".......".toCharArray(), 
+			".......".toCharArray()
+		};
+		
+		setBoard(board);
+		assertEquals(false, winOf('O'));
+		assertEquals(true, winOf('X'));
+	}
+
+	@Test
+	public void testWinOf_4() throws Exception {
+		
+		char[][] board = { 
+			".....O.".toCharArray(),
+			"....O..".toCharArray(),
+			"...O...".toCharArray(),
+			"..O....".toCharArray(),
+			".......".toCharArray(), 
+			".......".toCharArray()
+		};
+		
+		setBoard(board);
+		assertEquals(true, winOf('O'));
+		assertEquals(false, winOf('X'));
+	}
+
+	@Test
+	public void testWinOf_5() throws Exception {
+		
+		char[][] board = { 
+			".......".toCharArray(),
+			"..X....".toCharArray(),
+			"...X...".toCharArray(),
+			"....X..".toCharArray(),
+			".....X.".toCharArray(), 
+			".......".toCharArray()
+		};
+		
+		setBoard(board);
+		assertEquals(false, winOf('O'));
+		assertEquals(true, winOf('X'));
+	}
+}
---
+++ Connect4/src/com/kenjih/connectfour/test/players/TestGreadyPlayer.java
@@ -0,0 +1,104 @@
+package com.kenjih.connectfour.test.players;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.kenjih.connectfour.main.players.GreadyPlayer;
+import com.kenjih.connectfour.main.players.Player;
+
+public class TestGreadyPlayer {
+	Player player;
+	
+	
+	@Before
+	public void setUp() throws Exception {
+		player = new GreadyPlayer();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetNextHand_1() {
+		player.setStone('O');
+		
+		char[][] board = {
+			".......".toCharArray(),
+			".......".toCharArray(),
+			".......".toCharArray(),
+			"O......".toCharArray(),
+			"O......".toCharArray(),
+			"O.X.XX.".toCharArray()
+		};
+		
+		assertEquals(0, player.getNextHand(board));
+	}
+
+	@Test
+	public void testGetNextHand_2() {
+		player.setStone('O');
+		
+		char[][] board = {
+			".......".toCharArray(),
+			".......".toCharArray(),
+			".......".toCharArray(),
+			".......".toCharArray(),
+			"O......".toCharArray(),
+			"OOX.XX.".toCharArray()
+		};
+		
+		assertEquals(3, player.getNextHand(board));
+	}
+
+	@Test
+	public void testGetNextHand_3() {
+		player.setStone('O');
+		
+		char[][] board = {
+			".......".toCharArray(),
+			".......".toCharArray(),
+			".......".toCharArray(),
+			"O.O....".toCharArray(),
+			".O..X..".toCharArray(),
+			"O.X.XX.".toCharArray()
+		};
+		
+		assertEquals(3, player.getNextHand(board));
+	}
+
+	@Test
+	public void testGetNextHand_4() {
+		player.setStone('O');
+		
+		char[][] board = {
+			".......".toCharArray(),
+			".......".toCharArray(),
+			".......".toCharArray(),
+			"...X.X.".toCharArray(),
+			"O..OXX.".toCharArray(),
+			"O.XOXX.".toCharArray()
+		};
+		
+		assertEquals(5, player.getNextHand(board));
+	}
+
+	@Test
+	public void testGetNextHand_5() {
+		player.setStone('O');
+		
+		char[][] board = {
+			".......".toCharArray(),
+			".......".toCharArray(),
+			"...O...".toCharArray(),
+			"....O.X".toCharArray(),
+			"OOO..OX".toCharArray(),
+			"O.X.XXX".toCharArray()
+		};
+		
+		assertEquals(2, player.getNextHand(board));
+	}
+}
